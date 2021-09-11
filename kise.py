2b24b5f77c04911a4c0e464a8b9c42998561c415
jacob.huang <jacob.huang@lionic.com>
2021-09-11 17:33:44 +0800

    Add "edit" function to modify encrypted file on-the-fly

--- kise.py
+++ kise.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
-"""
-Usage:
-    kise.py [-h] [-d FILE | -e FILE]
-
-Options:
-    -h, --help      Show help message
-    -d, --decrypt   Decrypt document
-    -e, --encrypt   Encrypt document
-"""
-
-from docopt import docopt
+from sys import argv
 from getpass import getpass
+import subprocess
+import tempfile
 
 BEGIN_CHAR = 0x20
 END_CHAR = 0x7E
@@ -54,29 +46,41 @@ class Vigenere_Cipher:
 
         return chr(enc_res)
 
+key = getpass()
+vc = Vigenere_Cipher(key)
 
-args = docopt(__doc__)
+if argv[1] == 'enc':
 
-if args['--encrypt']:
+    with open(argv[2]) as f:
+        for c in f.read():
+            print(vc.encode(c), end='')
 
-    key = getpass()
-    vc = Vigenere_Cipher(key)
+elif argv[1] == 'dec':
 
-    with open(args['FILE']) as f:
-        s = f.read()
+    with open(argv[2]) as f:
+        for c in f.read():
+            print(vc.decode(c), end='')
 
-    for c in s:
-        print(vc.encode(c), end='')
+elif argv[1] == 'edit':
 
+    with tempfile.NamedTemporaryFile(delete=True) as tmp:
 
-elif args['--decrypt']:
+        plain = ''
+        with open(argv[2]) as f:
+            for c in f.read():
+               plain += vc.decode(c)
 
-    key = getpass()
-    vc = Vigenere_Cipher(key)
+        tmp.write(plain.encode())
+        tmp.seek(0)
 
-    with open(args['FILE']) as f:
-        s = f.read()
+        proc = subprocess.Popen(['vim', tmp.name], close_fds=True)
+        proc.communicate()
 
-    for c in s:
-        print(vc.decode(c), end='')
+        enc = ''
+        vc2 = Vigenere_Cipher(key)
+        for c in tmp.read():
+            enc += vc2.encode(chr(c))
+
+        with open(argv[2], 'wb') as f:
+            f.write(enc.encode())
 
