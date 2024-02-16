11a585ceab9c27e15c35aa01c91a01cd91c3c2d3
jacob.huang <tjhuang.lec@gmail.com>
2024-02-16 22:01:13 +0800

    Use Fernet module to encrypt data with salted key.
    1. Support unicode data encryption
    2. Enhance security by using salted key authentication

---
+++ README.md
@@ -0,0 +1,14 @@
+KISE means Keep It Simple Encrypt.
+
+
+## Encrypt file
+$ kise.py enc -s test.txt -d test.enc
+Password:
+
+## Decrypt file
+$ kise.py dec -s test.enc
+Password:
+Hello World
+
+## Edit encrypted file
+$ kise.py edit -s test.enc
--- kise.py
+++ kise.py
@@ -1,86 +1,154 @@
 #!/usr/bin/env python3
 
-from sys import argv
-from getpass import getpass
+import argparse
+import base64
+import os
 import subprocess
 import tempfile
 
-BEGIN_CHAR = 0x20
-END_CHAR = 0x7E
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from getpass import getpass
 
-class Vigenere_Cipher:
+class Kise:
 
-    def __init__(self, key):
+    def __init__(self, pwd=b'', salt=b''):
+        self.key = b''
+        self.pwd = pwd if pwd != b'' else getpass().encode()
+        self.salt = salt if salt != b'' else os.urandom(16)
+        self.calc_secret_key()
 
-        self.key = list(key)
-        self.index = 0
+    def calc_secret_key(self):
+        """ Calculate secret key from salt and password
+        """
 
-    def encode(self, data):
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), 
+                         length=32, 
+                         salt=self.salt, 
+                         iterations=400000)
 
-        d = ord(data);
-        k = ord(self.key[self.index]) - BEGIN_CHAR
-        self.index = (self.index + 1) % len(self.key)
+        self.key = base64.urlsafe_b64encode(kdf.derive(self.pwd))
 
-        if d < BEGIN_CHAR or d > END_CHAR:
-            return data
+    def encrypt(self, data: bytes) -> bytes:
+        """ Use secret key to encrypt data.
 
-        enc_res = d + k
-        if enc_res > END_CHAR:
-            enc_res = enc_res - (END_CHAR + 1) + BEGIN_CHAR
+        Args:
+            data: Data in byte format that want to encrypt
 
-        return chr(enc_res)
+        Returns:
+            The encrypted bytes
+        """
+
+        fernet = Fernet(self.key)
+
+        return fernet.encrypt(data)
+
+    def encrypt_file(self, src_file: str, dst_file: str):
+        """ Thansform data in src_file to be encrypted and store in dst_file.
+
+        Args:
+            src_file: The original plaintext file
+            dst_file: File to store ciphertext
+        """
+
+        with open(src_file, 'rb') as f:
+            enc_data = self.encrypt(f.read())
+
+        with open(dst_file, 'wb') as f:
+            salt_b64 = base64.b64encode(self.salt)
+            f.write(salt_b64 + b',' + enc_data)
+
+    def decrypt(self, enc_data: bytes) -> bytes:
+        """ Use secret key to decrypt data.
+
+        Args:
+            enc_data: Data in byte format that want to decrypt
+
+        Returns:
+            The decrypted bytes
+
+        Raises:
+            Exception that means invalid password
+        """
+
+        fernet = Fernet(self.key)
+
+        try:
+            return fernet.decrypt(enc_data)
+        except:
+            raise Exception('Incorrect password')
 
 
-    def decode(self, data):
+    def decrypt_file(self, src_file, dst_file=None):
+        """ Thansform data in src_file to be decrypted and store in dst_file.
 
-        d = ord(data);
-        k = ord(self.key[self.index]) - BEGIN_CHAR
-        self.index = (self.index + 1) % len(self.key)
+        If dst_file is None, then show the contents on standard output.
 
-        if d < BEGIN_CHAR or d > END_CHAR:
-            return data
+        Args:
+            src_file: The original plaintext file
+            dst_file: File to store ciphertext
+        """
 
-        enc_res = d - k
-        if enc_res < BEGIN_CHAR:
-            enc_res = enc_res - BEGIN_CHAR + (END_CHAR + 1)
+        with open(src_file, 'rb') as f:
+            salt_b64, enc_data = f.read().decode().split(',')
 
-        return chr(enc_res)
+            self.salt = base64.b64decode(salt_b64)
+            enc_data = enc_data.encode()
 
-key = getpass()
-vc = Vigenere_Cipher(key)
+            self.calc_secret_key()
 
-if argv[1] == 'enc':
+        data = self.decrypt(enc_data)
 
-    with open(argv[2]) as f:
-        for c in f.read():
-            print(vc.encode(c), end='')
+        if dst_file:
+            with open(dst_file, 'wb') as f:
+                f.write(data)
+        else:
+            print(data.decode(), end='')
 
-elif argv[1] == 'dec':
 
-    with open(argv[2]) as f:
-        for c in f.read():
-            print(vc.decode(c), end='')
+def enc_handler(args):
+    k = Kise()
+    k.encrypt_file(args.src_file, args.dst_file)
 
-elif argv[1] == 'edit':
+def dec_handler(args):
+    k = Kise()
+    k.decrypt_file(args.src_file, args.dst_file)
 
+def edit_handler(k, args):
+    k = Kise()
     with tempfile.NamedTemporaryFile(delete=True) as tmp:
 
-        plain = ''
-        with open(argv[2]) as f:
-            for c in f.read():
-               plain += vc.decode(c)
-
-        tmp.write(plain.encode())
-        tmp.seek(0)
+        k.decrypt(args.src_file, tmp.name)
 
         proc = subprocess.Popen(['vim', tmp.name], close_fds=True)
         proc.communicate()
 
-        enc = ''
-        vc2 = Vigenere_Cipher(key)
-        for c in tmp.read():
-            enc += vc2.encode(chr(c))
+        k.encrypt(tmp.name, args.src_file)
 
-        with open(argv[2], 'wb') as f:
-            f.write(enc.encode())
+def parse_argument():
 
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='operation')
+
+    dec_parser = subparser.add_parser('dec', help='Decrypt file')
+    dec_parser.add_argument('-s', '--src_file', required=True)
+    dec_parser.add_argument('-d', '--dst_file')
+    dec_parser.set_defaults(func=dec_handler)
+
+    enc_parser = subparser.add_parser('enc', help='Encrypt file')
+    enc_parser.add_argument('-s', '--src_file', required=True)
+    enc_parser.add_argument('-d', '--dst_file', required=True)
+    enc_parser.set_defaults(func=enc_handler)
+
+    edit_parser = subparser.add_parser('edit', help='Edit encrypted file')
+    edit_parser.add_argument('-s', '--src_file', required=True)
+    edit_parser.set_defaults(func=edit_handler)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = parse_argument()
+    args.func(args)
